d2fda9232e1c19e4ec06ee1372ed8fe6215a3599
Stacy <aleyostacy@gmail.com>
2024-10-02 11:03:18 -0700

    made some changes

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// Assignment 1
+const names = ['Alice', 'Bob', 'Charlie'];
+const event = 'birthday';
+console.log(writeCards(names, event));
+
+function writeCards(names, event) {
+  let message = [];
+
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the amazing ${event} gift!`);
+  }
+
+  return message;
+}
+
+//Assignment 2
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number); 
+    number--; 
+  }
+}
